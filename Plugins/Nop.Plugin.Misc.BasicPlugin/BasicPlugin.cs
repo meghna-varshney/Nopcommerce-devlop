8a8bbb9a67ce9a2b9c3bb57070fb26cbcba1795a
meghna-varshney <33701517+meghna-varshney@users.noreply.github.com>
2020-02-07 12:57:32 +0530

    new plugin

---
+++ Plugins/Nop.Plugin.Misc.BasicPlugin/BasicPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.BasicPlugin
+{
+    public class BasicPlugin : BasePlugin
+    {
+        public override void Install()
+        {
+            base.Install();
+        }
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+    }
+}
